92d51567dd34396c600dd1d37e0a261c55d8de22
DonkeYSpotteD <alienjiad@gmail.com>
2025-10-17 18:51:00 +0800

    upates 3

--- stringarray.c
+++ stringarray.c
@@ -3,9 +3,9 @@ int main()
 {
     char arr[]="ashfaq dont have any dick";
     int i=0;;
-    while(i<100000)
+    while()
     {
-    printf("%s \n ",arr);
+    printf("%s\n ",arr);
     i++;
     }
         return 0;  
