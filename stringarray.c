5a7188280e81e52c384ccc29cd8aeb081c4c534f
DonkeYSpotteD <alienjiad@gmail.com>
2025-10-17 18:35:43 +0800

    updates2

---
+++ stringarray.c
@@ -0,0 +1,12 @@
+#include<stdio.h>   
+int main()
+{
+    char arr[]="ashfaq dont have any dick";
+    int i=0;;
+    while(i<100000)
+    {
+    printf("%s \n ",arr);
+    i++;
+    }
+        return 0;  
+}    
